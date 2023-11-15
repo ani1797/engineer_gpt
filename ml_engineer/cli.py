35246cd6408fe4064ceb1dec16463e25bf4a3869
Anirudh Aggarwal <ani1797@users.noreply.github.com>
2023-11-14 22:57:52 -0500

    Update cli.py

--- ml_engineer/cli.py
+++ ml_engineer/cli.py
@@ -24,25 +24,6 @@ def cli(*args):
     parser.add_argument("--model", type=str, default="gpt35", help="The engine to use for the completion")
     return parser.parse_args(args)
 
-import re
-
-def extract_filename_and_codeblock(markdown_text):
-    # Regex pattern for filename
-    filename_pattern = r'```markdown\n(.*?)\n```'
-    # Regex pattern for code block
-    codeblock_pattern = r'```(?:[a-z]+\n)?(.*?)```'
-
-    # Find filename
-    filename = re.search(filename_pattern, markdown_text, re.DOTALL)
-    filename = filename.group(1) if filename else None
-
-    # Find code block
-    codeblock = re.search(codeblock_pattern, markdown_text, re.DOTALL)
-    codeblock = codeblock.group(1) if codeblock else None
-
-    return filename, codeblock
-
-
 if __name__ == "__main__":
     config = cli(*sys.argv[1:])
     ai = OpenAI(model=config.model)
